7bb5454c8538b87447ebbf5c3dbfe669e601a5bc
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2022-07-11 20:24:31 -0700

    11th-commit

--- e_project/core/urls.py
+++ e_project/core/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('login', views.login_view, name='login'),
     path('account/register/', views.register_view, name='register'),
+    path('', views.dashboard_view, name='dashboard'),
 ]
